a31c3811b6ad63a0a95e5f32ca634eac36057c44
0xsufi <0xsufi@noma.money>
2025-05-08 21:53:53 -0400

    add home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,13 @@ import { Container } from "@chakra-ui/react";
 // import { commify } from "../utils";
 
 // Components
-import Stats from "../components/Stats.tsx";
-import Treasury from "../components/Treasury.tsx";
-import AMM from "../components/AMM.tsx";
-import ALM from "../components/ALM.tsx";
-import CTA from "../components/CTA.tsx";
+// import Stats from "../components/Stats.tsx";
+// import Treasury from "../components/Treasury.tsx";
+// import AMM from "../components/AMM.tsx";
+// import ALM from "../components/ALM.tsx";
+// import CTA from "../components/CTA.tsx";
 import Hero from "../components/Hero.tsx";
-import Details from "../components/Details.tsx";
+// import Details from "../components/Details.tsx";
 
 
 const HomePage: React.FC = () => {
@@ -17,12 +17,12 @@ const HomePage: React.FC = () => {
     <>
       <Container>
         <Hero />
-        <Stats />
+        {/* <Stats />
         <Treasury />
         <Details />
         <ALM />
         <AMM />
-        <CTA />
+        <CTA /> */}
       </Container>
     </>
   );
