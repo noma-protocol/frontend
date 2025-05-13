44e0d7d09b282d37dcb9010f98194db3d4c7c04c
0xSufi <0xsufi@noma.money>
2025-05-12 20:08:01 -0400

    config file

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+const presaleContractAddress = "0xd582883e944fb36A2Be30f478e58e33339c01426";
+const environment = import.meta.env.VITE_ENV;
+const chain = import.meta.env.VITE_CHAIN;
+
+export default {
+    chain,
+    environment,
+    presaleContractAddress
+};
+
