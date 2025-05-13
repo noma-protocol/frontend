baba58223b5591a480d3d56dd670e8a26ddb8f37
0xsufi <0xsufi@noma.money>
2025-05-12 22:21:36 -0400

    improve resp. style wip

--- src/components/LiquidityChart.tsx
+++ src/components/LiquidityChart.tsx
@@ -26,7 +26,7 @@ return (
         {/* <SimpleGrid maxWidth={"60%"} ml="20%" columns={8}>
           {isConnected ? <SlideControls isConnected /> : <></>}
         </SimpleGrid>*/}
-        <SimpleGrid maxWidth={isMobile ? "40%" : "30%"} ml="50vw" columns={2} rows={3} p={2} mt={isMobile ? "-6vh" : 0}  >
+        <SimpleGrid maxWidth={isMobile ? "40%" : "30%"} ml={isMobile ? "50vw" : "40vw"} columns={2} rows={3} p={2} mt={isMobile ? "-6vh" : "-100px"}  >
           <Box w="auto" textAlign="right"><Text color="#a67c00" fontSize={isMobile?"10px":"14px"}>Circulating</Text></Box>
           <Box w="auto" textAlign="right"><Text color="#a67c00" fontSize={isMobile?"10px":"14px"}>{isMobile ? "L.R." : "Liquidity Ratio"}</Text></Box>
           <Box w="auto" textAlign="right" fontSize={isMobile?"11px":"14px"}>
