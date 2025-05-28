3d6c2fe0a93e235ea66ba377572c4e80482e28a2
0xsufi <0xsufi@noma.money>
2025-05-28 00:28:07 -0400

    attempt to fix style issue

--- src/pages/Borrow.tsx
+++ src/pages/Borrow.tsx
@@ -697,10 +697,10 @@ const Borrow = () => {
                             page="borrow"
                             />
                         </Box>
-                        <Box p={2} mt={5} w={isMobile ? "92%" : "98%"} ml={-5} ml={-5} border="1px solid ivory" borderRadius={10} backgroundColor={"#222831"} >
+                        <Box p={2} mt={5} w={isMobile ? "90%" : "98%"} ml={-5} ml={-5} border="1px solid ivory" borderRadius={10} backgroundColor={"#222831"} >
                             <Text fontSize={"12px"} fontWeight={"bold"} color="#a67c00" ml={2}>Active Loan</Text>        
                            <SimpleGrid columns={4} mt={-5} backgroundColor={"#222831"} w={isMobile ? "94%" : "352px"} ml={2} mr={2}>
-                                <Box fontSize="xs" w='95px' border="1px solid red" px={2} color="white" backgroundColor={"#a67c00"}> Collateral </Box>
+                                <Box fontSize="xs" w='95px' px={2} color="white" backgroundColor={"#a67c00"}> Collateral </Box>
                                 <Box fontSize="xs" px={2} color="white" backgroundColor={"#a67c00"}> Borrowed </Box>
                                 <Box fontSize="xs" px={2} color="white" backgroundColor={"#a67c00"}> 
                                 <HStack>
