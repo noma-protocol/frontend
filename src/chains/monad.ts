b0234857620f98b1866d7408df205d4b499db839
oikos1 <albert@oikos.cash>
2025-05-29 20:19:06 -0400

    add wagmi monad chain

---
+++ src/chains/monad.ts
@@ -0,0 +1,28 @@
+import { type Chain } from 'viem'
+
+export const monad = {
+  id: 10143,
+  name: 'Monad Testnet',
+  nativeCurrency: {
+    name: 'Monad',
+    symbol: 'MON',
+    decimals: 18,
+  },
+  rpcUrls: {
+    public: {
+      http: ['https://testnet-rpc.monad.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Monad Explorer',
+      url: 'https://testnet.monadexplorer.com/',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 251449,
+    },
+  },
+} as const satisfies Chain
