967e690f1c2a58591e7ee7282d736db04073d8aa
0xsufi <0xsufi@noma.money>
2025-06-08 19:20:40 -0400

    add navigation, style

--- src/components/LiquidityChart.tsx
+++ src/components/LiquidityChart.tsx
@@ -29,7 +29,7 @@ return (
         <SimpleGrid 
           maxWidth={isMobile ? "40%" : "20%"} 
           ml={isMobile ? "50vw" : "50vw"} 
-          mt={isMobile ? "-6vh" : "-100px"} 
+          mt={isMobile ? "-11vh" : "-160px"} 
           columns={2} 
           rows={3} 
           p={2}   
