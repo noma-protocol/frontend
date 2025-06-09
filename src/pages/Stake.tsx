122274ed018b7f2da2b8443d5f52d4da5afb302e
0xSufi <0xsufi@noma.money>
2025-06-09 01:55:36 -0400

    normalize grid size

--- src/pages/Stake.tsx
+++ src/pages/Stake.tsx
@@ -653,7 +653,7 @@ const Stake = () => {
                                 backgroundColor={"#222831"} 
                             >
                             <Text fontSize={"xs"} fontWeight={"bold"} color="#a67c00">New Position</Text>
-                            <SimpleGrid columns={2}  w={isMobile ? "96%" : "352px"}  mt={-5} fontSize={"xs"}>
+                            <SimpleGrid columns={2}  w={isMobile ? "98%" : "352px"}  mt={-5} fontSize={"xs"}>
                                 <Box  w="auto" backgroundColor={"#bf9b30"}  mb={2}>
                                     <Text fontSize="xs">&nbsp;<b>Amount</b></Text>
                                 </Box>
